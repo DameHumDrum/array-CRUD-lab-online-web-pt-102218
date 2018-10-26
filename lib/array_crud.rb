555ad6104db176c91b65d8450e3a2f154dc90004
Katie Ashlin <akka726@outlook.com>
2018-10-26 03:28:08 +0000

    automatically backed up by learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -8,7 +8,7 @@ end
 
 def add_element_to_end_of_array(array, element)
   favorite_anime = ["My Hero Academia", "Attack on Titan","Cowboy Bebop","Dragonball Z"]
-  favorite_anime.push("Sailor Moon")
+  favorite_anime.push("arrays!")
 end
 
 def add_element_to_start_of_array(array, element)
